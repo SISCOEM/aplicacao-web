94af251c4a702b22abb21c00079bb68f1d733fb4
Anthonius <anthoniusmiguel@gmail.com>
2024-06-17 18:51:58 -0300

    Iniciando construção da API

--- sicomb/api_rest/views.py
+++ sicomb/api_rest/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
 from django.contrib.auth.hashers import check_password 
@@ -11,13 +10,14 @@ def LoginAPIView(request):
         try:
             police = Police.objects.get(matricula=registration)
         except Police.DoesNotExist:
-            return Response({'error_message': "Matrícula incorreta"})
+            return Response({'error_message': "Matrícula incorreta"}, status=401)
         if check_password(password, police.password):
-            
-        print(user)
-        if user:
-            token, _ = Token.objects.get_or_create(user=user)
+            if not police.activated or police.activator is None:
+                return Response({'error_message': "Matrícula incorreta"}, status=400)
+                        
+            token, _ = Token.objects.get_or_create(user=police)
             return Response({'token': token.key})
+        
         else:
             return Response({'error': 'Credenciais incorretas'}, status=400)
         
