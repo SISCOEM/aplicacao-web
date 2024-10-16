5d13f7ba62e1af42b1ffcd8baa8fe1a1949d6f08
lauf8 <lucasafonsodesouza@gmail.com>
2024-10-16 10:50:20 -0300

    feat: removendo comandos que não devem ser feitos localmente

--- sicomb/entrypoint.sh
+++ sicomb/entrypoint.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
-sleep 4
-
-# Comandos adicionais do Django
-# python3 /code/init_sql.py
-python manage.py migrate --fake contenttypes
-python manage.py migrate
 python manage.py collectstatic --noinput
 
-python3 /code/execute_sql.py
-# Executa o comando padrão
 exec "$@"
 
